f0a798367eb45a58d10d7cf367f98b86cafd55ff
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-11-13 18:37:45 +0000

    add slash in web requests

--- web/requests.ts
+++ web/requests.ts
@@ -2,14 +2,14 @@ const { origin, pathname } = window.location;
 const BASE_URL = `${origin}${pathname}`;
 
 export async function getRainbows(roomId: string) {
-    const rainbowsResponse = await fetch(`${BASE_URL}api/rainbows?roomId=${roomId}`);
+    const rainbowsResponse = await fetch(`${BASE_URL}/api/rainbows?roomId=${roomId}`);
     const rainbowsResult = await rainbowsResponse.json();
 
     return rainbowsResult;
 }
 
 export async function postRainbow(roomId: string) {
-    const rainbowsResponse = await fetch(`${BASE_URL}api/rainbow?roomId=${roomId}`, {
+    const rainbowsResponse = await fetch(`${BASE_URL}/api/rainbow?roomId=${roomId}`, {
         method: "POST",
         body: JSON.stringify({})
     });
